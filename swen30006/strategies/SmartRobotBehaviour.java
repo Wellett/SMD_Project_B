e726d56f84623a73c2ea9c9c6216e75b3e6cf258
William <wellett@student.unimelb.edu.au>
2017-09-17 23:35:58 +1000

    remove the tempTube from SmartRobotBehaviour

--- swen30006/strategies/SmartRobotBehaviour.java
+++ swen30006/strategies/SmartRobotBehaviour.java
@@ -63,22 +63,22 @@ public class SmartRobotBehaviour implements IRobotBehaviour{
 	@Override
 	public boolean fillStorageTube(IMailPool mailPool, StorageTube tube) {
 
-		ArrayList<MailItem> tempTube = new ArrayList<MailItem>();
+		//ArrayList<MailItem> tempTube = new ArrayList<MailItem>();
 
 		// Empty my tube
-		while(!tube.tube.isEmpty()){
+		while(!tube.isEmpty()){
 			mailPool.addToPool(tube.pop());
 		}
 
 		// Grab priority mail
-		while(tempTube.size() < tube.MAXIMUM_CAPACITY){
+		while(tube.size() < tube.MAXIMUM_CAPACITY){
 			if(containMail(mailPool,MailPool.PRIORITY_POOL)){
-				tempTube.add(mailPool.getHighestPriorityMail());
+				tube.add(mailPool.getHighestPriorityMail());
 			}
 			else{
 				// Fill it up with non priority
 				if(containMail(mailPool,MailPool.NON_PRIORITY_POOL)){
-					tempTube.add(mailPool.getNonPriorityMail());
+					tube.add(mailPool.getNonPriorityMail());
 				}
 				else{
 					break;
@@ -87,19 +87,19 @@ public class SmartRobotBehaviour implements IRobotBehaviour{
 			}
 		}
 
-		// Sort tempTube based on floor
+		// Sort tube based on floor
 		// This actually sorts based on arrival time?
 		//make a note of this in report, DON'T change behaviour
-		tempTube.sort(new ArrivalComparer());
+		tube.sort(new ArrivalComparer());
 
-		// Iterate through the tempTube
-		while(tempTube.iterator().hasNext()){
-			try {
-				tube.addItem(tempTube.remove(0));
-			} catch (TubeFullException e) {
-				e.printStackTrace();
-			}
-		}
+		// // Iterate through the tempTube
+		// while(tempTube.iterator().hasNext()){
+		// 	try {
+		// 		tube.addItem(tempTube.remove(0));
+		// 	} catch (TubeFullException e) {
+		// 		e.printStackTrace();
+		// 	}
+		// }
 
 		// Check if there is anything in the tube
 		if(!tube.tube.isEmpty()){
